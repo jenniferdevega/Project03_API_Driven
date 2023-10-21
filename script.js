c42c4519d8953c239e74a96c05f1fcd70cdd0853
jenniferdevega <devega.jennifer.alvarez@gmail.com>
2023-10-21 23:36:10 +0800

    Added event listeners for onload, search button and validation

---
+++ script.js
@@ -0,0 +1,43 @@
+//--===================== Start of Declaration =====================--
+
+
+//--===================== End of Declaration =====================--
+
+//--===================== Start of Execution=====================--
+//First execution
+window.addEventListener('load', () => {
+    header();
+  })
+
+//Event Listeners
+btnSearchLocation.addEventListener('click', () => {
+  validateInputLocation();
+})
+//--===================== End of Execution =====================--
+
+//--===================== Start of Functions =====================--
+function header() {
+    const header = document.querySelector("#header");
+    header.innerHTML += 
+    `
+      <h1 class="text-center p-3">
+        Weather Forecast
+      </h1>
+    `;
+  }
+
+//Validation
+function validateInputLocation(){
+  const inputLocation = document.querySelector("#inputLocation").value.toUpperCase();
+  if(inputLocation =="" || !inputLocation.match(/\w/)){
+    Swal.fire({
+      title: 'Location Not Found',
+      icon: 'error',
+      html: '<span style="color:#0461B1">Please enter the correct <strong>Location Name</strong></span>',
+      allowOutsideClick: false
+    });
+  }else{
+    alert("hello");
+  }
+}
+//--===================== Start of End =====================--
